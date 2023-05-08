d89a1954da81d76b4467098709c50f8ab9281bb7
f-safa <f.safa2001@gmail.com>
2023-05-08 18:31:13 +0330

    add java

---
+++ index.js
@@ -0,0 +1,69 @@
+//1
+let now = new Date();
+
+let dates = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"];
+let today = dates[now.getDay()];
+
+let currentTime = now.getHours();
+if (currentTime < 10) {
+  currentTime = `0${currentTime}`;
+}
+let currentMinutes = now.getMinutes();
+if (currentMinutes < 10) {
+  currentMinutes = `0${currentMinutes}`;
+}
+
+document.querySelector(
+  "#current-date"
+).innerHTML = `${today} ${currentTime} : ${currentMinutes}`;
+
+//2
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temprature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+function searchCity(city) {
+  let apiKey = "50fa4024e3b1d5eac2f51ab18a47e997";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+function searchLocation(position) {
+  let apiKey = "50fa4024e3b1d5eac2f51ab18a47e997";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("new york");
+
+//3
+
+function change(event) {
+  event.preventDefault();
+  let tempratureelement = document.querySelector("#temprature");
+  let temprature = tempratureelement.innerHTML;
+  temprature = Number(temprature);
+  tempratureelement.innerHTML = Math.round(temprature * 1.8 + 32);
+}
